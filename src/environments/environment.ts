0a5651bf888e7e1d5b2185566b65e60d1a56be73
Krexzus <kennardhoran@gmail.com>
2026-02-01 23:36:12 -0600

    Resolve environment conflict and add date-fns dependency

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,9 @@
-export const environment = {
+﻿export const environment = {
   production: false,
   supabaseUrl: 'https://ngqcsposkknbmzgnalba.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ncWNzcG9za2tuYm16Z25hbGJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMTI4MTYsImV4cCI6MjA4NDU4ODgxNn0.14VFmURKH0VBoi5--7UFWQj5wmHkXoveXdIVkkQEQFE',
-<<<<<<< HEAD
   // 20 prompts cada api, asi que alisten las 30 apis https://aistudio.google.com/app/apikey
   geminiApiKey: 'AIzaSyBe1-3wbREKEEHQRCvXH22nVnlXeqNl8LQ',
   paypalClientId: 'AezLcprAi-_J_q1OwQWWbMIPJ_xJexjWCqqb3E_BfgGKxObceJmaXukwY3kVrIpRkqhe-vOvgE759J3E',
   paypalCurrency: 'MXN'
-=======
-  // Gemini API Key - Obtén tu key en https://aistudio.google.com/app/apikey
-  geminiApiKey: 'AIzaSyD_y0yu7dwyB1ueq5vWK467INphrG85tM4'
->>>>>>> 09bf28a4ed83099106cf4632568525fad30fc44b
 };
